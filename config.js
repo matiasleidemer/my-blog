e5e847e51e7de1d43fc297d0193d07073d84b715
Matias Leidemer <matiasleidemer@gmail.com>
2019-12-01 22:10:46 -0300

    Update blog with my information

--- config.js
+++ config.js
@@ -1,47 +1,43 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'http://www.leidemer.com',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  title: `Matias Leidemer's Blog`,
+  subtitle: 'Hi, my name is Matias. I am a software developer',
   copyright: '© All rights reserved.',
   disqusShortname: '',
-  postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  postsPerPage: 10,
+  googleAnalyticsId: 'UA-144702319-1',
   useKatex: false,
   menu: [
     {
-      label: 'Articles',
+      label: 'Blog',
       path: '/'
     },
     {
-      label: 'About me',
-      path: '/pages/about'
-    },
-    {
-      label: 'Contact me',
-      path: '/pages/contacts'
+      label: 'About',
+      path: '/about'
     }
   ],
   author: {
-    name: 'John Doe',
+    name: 'Matias Leidemer',
     photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    bio: 'My name is Matias, I am a software developer from Novo Hamburgo, Brazil',
     contacts: {
-      email: '',
-      facebook: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
-      rss: '',
-      vkontakte: '',
-      linkedin: '#',
-      instagram: '#',
-      line: '',
+      codepen: '',
+      email: 'matiasleidemer@gmail.com',
+      facebook: '',
+      github: 'matiasleidemer',
       gitlab: '',
-      weibo: '',
-      codepen: ''
+      instagram: '',
+      line: '',
+      linkedin: 'matiasleidemer',
+      rss: '',
+      telegram: '',
+      twitter: 'matiasleidemer',
+      vkontakte: '',
+      weibo: ''
     }
   }
 };
---
+++ content/about.md
@@ -0,0 +1,14 @@
+---
+title: "About"
+template: "page"
+---
+
+Hello.
+
+My name is Matias Leidemer, I am a software developer based in Novo Hamburgo, Brazil. I am currently working for [Stickermule](https://www.stickermule.com).
+
+I like to write code that writes code, listen to music, watch movies and all kinds of animals.
+
+This page is powered by [Gatsby](https://www.gatsbyjs.org/), with the [lumen theme](https://www.gatsbyjs.org/starters/alxshelepenok/gatsby-starter-lumen/) and hosted using [Netlify](https://www.netlify.com).
+
+If you feel like talking to me, you can use one of the social links at the left of this page.
